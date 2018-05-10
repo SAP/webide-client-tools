33164d4af5efbc1cef6d59aee1c451c389433f57
Soel <shachar.soel@sap.com>
2018-05-10 13:04:41 +0300

    Cleanup karma-ci
    Fixes #33

--- example/template/scripts/karma_ci.js
+++ example/template/scripts/karma_ci.js
@@ -1,27 +1,16 @@
-"use strict"
-
 const { Server, config } = require("karma")
 const path = require("path")
 
-// TODO: detect travis too?
-if (process.env.XMAKE_TOOL_INSTALLATION_ROOT) {
-    console.log(
-        "Running in xMake mode. Karma tests will not be executed as no browsers are available..."
-    )
-} else {
-    const karmaConfig = config.parseConfig(
-        path.resolve(__dirname, "../karma.conf.js")
-    )
+const karmaConfig = config.parseConfig(
+  path.resolve(__dirname, "../karma.conf.js")
+)
 
-    // CI only configurations
-    karmaConfig.singleRun = true
-    // Unable to get latest Chrome working on SAP's Travis
-    // Will retry when SAP's Travis upgrades infrastructure.
-    karmaConfig.browsers = ["Firefox"]
+// CI only configurations
+karmaConfig.singleRun = true
+karmaConfig.browsers = ["ChromeHeadless"]
 
-    const server = new Server(karmaConfig, exitCode => {
-        console.error(`Karma has exited with ${exitCode}`)
-        process.exit(exitCode)
-    })
-    server.start()
-}
+const server = new Server(karmaConfig, exitCode => {
+  console.error(`Karma has exited with ${exitCode}`)
+  process.exit(exitCode)
+})
+server.start()
