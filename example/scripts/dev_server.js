fe1c9c3cebb83d1b0096a91532cbb950245678be
Tal Sapan <tal.sapan@sap.com>
2018-05-15 17:34:08 +0300

    POC - Changes in feature

--- example/scripts/dev_server.js
+++
@@ -1,4 +0,0 @@
-const clientTools = require("@sap-webide/webide-client-tools")
-
-// <startConnect> has an optional options argument which can be used to customize for different scenarios
-clientTools.devServer.startConnect()
